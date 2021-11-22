96bbe085813d1a8c570944aeba46053f2d62f315
wangzeyu <wangzeyu@kylinos.cn>
2021-11-22 21:01:49 +0800

    init project
    Signed-off-by: wangzeyu <wangzeyu@kylinos.cn>

---
+++ include/kysec_file_restore.h
@@ -0,0 +1,14 @@
+#ifndef _KYSEC_FILE_RESTROE_
+#define _KYSEC_FILE_RESTROE_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void test_func();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //_KYSEC_FILE_RESTROE_
---
+++ lib/kysec_file_sync.c
@@ -0,0 +1,7 @@
+#include "kysec_file_restore.h"
+#include <stdio.h>
+
+void test_func()
+{
+    printf("this is test_func\n");
+}
